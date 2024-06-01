8e0ecad8e0e4b14811008c6d3fe11c92f218954f
khanhtung97 <nguyenkhanhtung580@gmail.com>
2024-06-01 08:01:43 +0700

    product search

--- ecommerceMVC/Controllers/HanghoaController.cs
+++ ecommerceMVC/Controllers/HanghoaController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ecommerceMVC.Data;
+using ecommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ecommerceMVC.Controllers
 {
     public class HanghoaController : Controller
     {
-        public IActionResult Index()
+        private readonly Hshop2023Context _context;
+
+        public HanghoaController(Hshop2023Context context)
         {
-            return View();
+            _context = context;
+        }
+
+        public IActionResult Index(int? loai)
+        {
+            var hangHoas = _context.HangHoas.AsQueryable();
+            if (loai.HasValue)
+            {
+                hangHoas = hangHoas.Where(p=>p.MaLoai == loai.Value);
+            }
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
+        }
+
+        public IActionResult Search(string? query)
+        {
+            var hangHoas = _context.HangHoas.AsQueryable();
+            if (query != null)
+            {
+                hangHoas = hangHoas.Where(p => p.TenHh.Contains(query));
+            }
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
         }
     }
 }
---
+++ ecommerceMVC/ViewModels/HangHoaVM.cs
@@ -0,0 +1,12 @@
+﻿namespace ecommerceMVC.ViewModels
+{
+    public class HangHoaVM
+    {
+        public int MaHh { get; set; }
+        public string TenHH { get; set; } = string.Empty;
+        public string Hinh { get; set; } = string.Empty;
+        public double DonGia { get; set; }
+        public string MoTaNgan { get; set; } = string.Empty;
+        public string TenLoai { get; set; } = string.Empty;
+    }
+}
